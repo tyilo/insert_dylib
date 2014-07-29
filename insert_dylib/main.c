b4a9de83c2cf4ebb302956708618d3e3eb0ff759
Asger Drewsen <asgerdrewsen@gmail.com>
2014-07-29 20:17:13 +0200

    Initial Commit

---
+++ insert_dylib/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  insert_dylib
+//
+//  Created by Asger Hautop Drewsen on 29/07/14.
+//  Copyright (c) 2014 Tyilo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
